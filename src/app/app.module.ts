119e117dff805d0312480b3336e0f4559f02c64d
Anil Kumar <kanil3727@gmail.com>
2022-05-04 14:44:52 +0530

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CreateToDoComponentComponent } from './create-to-do-component/create-to-do-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateToDoComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
